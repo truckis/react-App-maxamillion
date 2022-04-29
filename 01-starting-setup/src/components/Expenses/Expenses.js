542e65c7e01d57a17adffbc568c275b5da8c9091
Truckis <truckis@Truckiss-MacBook-Pro.local>
2022-04-28 20:17:45 -0700

    Dynamic rendering using map() in Expenses.js

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -17,16 +17,13 @@ function Expenses(props){
         <div>
             <Card className='expenses'>
             <ExpensesFilter selected={filteredYear} onFilterChange={filterChangeHandler} />
+                {props.item.map((expense) => 
                 <ExpenseItem 
-                    title={props.item[0].title} 
-                    amount={props.item[0].amount} 
-                    date={props.item[0].date}>
-                </ExpenseItem>
-                <ExpenseItem 
-                    title={props.item[1].title} 
-                    amount={props.item[1].amount} 
-                    date={props.item[1].date}>
-                </ExpenseItem>
+                    title={expense.title} 
+                    amount={expense.amount} 
+                    date={expense.date} />
+                    )
+                }
             </Card>
         </div>
     )
